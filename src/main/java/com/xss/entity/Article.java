df61907021f77227de69685c83bc0357f0f712a4
Xie <1501879136@qq.com>
2020-08-28 09:36:10 +0800

    增加代码

---
+++ src/main/java/com/xss/entity/Article.java
@@ -0,0 +1,182 @@
+package com.xss.entity;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Table(name = "article")
+public class Article {
+    @Id
+    private Integer id;
+
+    /**
+     * 文章标题
+     */
+    private String title;
+
+    /**
+     * 文章内容
+     */
+    private String content;
+
+    /**
+     * 浏览次数
+     */
+    @Column(name = "browse_count")
+    private Integer browseCount;
+
+    /**
+     * 发布时间
+     */
+    @Column(name = "publish_date")
+    private Date publishDate;
+
+    /**
+     * 发布人名称
+     */
+    @Column(name = "publish_real_name")
+    private String publishRealName;
+
+    /**
+     * 发布人id
+     */
+    @Column(name = "user_id")
+    private Integer userId;
+
+    private Integer look;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取文章标题
+     *
+     * @return title - 文章标题
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * 设置文章标题
+     *
+     * @param title 文章标题
+     */
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    /**
+     * 获取文章内容
+     *
+     * @return content - 文章内容
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * 设置文章内容
+     *
+     * @param content 文章内容
+     */
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+
+    /**
+     * 获取浏览次数
+     *
+     * @return browse_count - 浏览次数
+     */
+    public Integer getBrowseCount() {
+        return browseCount;
+    }
+
+    /**
+     * 设置浏览次数
+     *
+     * @param browseCount 浏览次数
+     */
+    public void setBrowseCount(Integer browseCount) {
+        this.browseCount = browseCount;
+    }
+
+    /**
+     * 获取发布时间
+     *
+     * @return publish_date - 发布时间
+     */
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    /**
+     * 设置发布时间
+     *
+     * @param publishDate 发布时间
+     */
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    /**
+     * 获取发布人名称
+     *
+     * @return publish_real_name - 发布人名称
+     */
+    public String getPublishRealName() {
+        return publishRealName;
+    }
+
+    /**
+     * 设置发布人名称
+     *
+     * @param publishRealName 发布人名称
+     */
+    public void setPublishRealName(String publishRealName) {
+        this.publishRealName = publishRealName == null ? null : publishRealName.trim();
+    }
+
+    /**
+     * 获取发布人id
+     *
+     * @return user_id - 发布人id
+     */
+    public Integer getUserId() {
+        return userId;
+    }
+
+    /**
+     * 设置发布人id
+     *
+     * @param userId 发布人id
+     */
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * @return look
+     */
+    public Integer getLook() {
+        return look;
+    }
+
+    /**
+     * @param look
+     */
+    public void setLook(Integer look) {
+        this.look = look;
+    }
+}
---
+++ src/main/java/com/xss/mapper/ArticleMapper.java
@@ -0,0 +1,7 @@
+package com.xss.mapper;
+
+import com.xss.entity.Article;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface ArticleMapper extends Mapper<Article> {
+}
